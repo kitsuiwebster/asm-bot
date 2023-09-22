b0cafec5e15e1b170faf34d209868031fb13a720
kitsuiwebster <raphmartin1999@gmail.com>
2023-09-23 00:01:37 +0200

    fix: typing indicator

--- commands/asm.py
+++ commands/asm.py
@@ -4,6 +4,7 @@ import openai
 import requests
 from discord.ext import commands
 from prompt import prompt
+import concurrent.futures
 
 class AsmCog(commands.Cog):
     def __init__(self, bot):
@@ -11,8 +12,6 @@ class AsmCog(commands.Cog):
 
     @commands.command()
     async def asm(self, ctx, *, user_query):
-
-
         print("Command received:", user_query)
         system_prompt = prompt
         headers = {
@@ -28,25 +27,30 @@ class AsmCog(commands.Cog):
             "max_tokens": 6000,
         }
 
-        # Start typing indicator
         async with ctx.typing():
-            response = requests.post(
-                "https://api.openai.com/v1/chat/completions",
-                headers=headers,
-                data=json.dumps(data),
-            )
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                response = await self.bot.loop.run_in_executor(
+                    executor, self.get_response, headers, data
+                )
+
             if response.status_code == 200:
+                response_json = response.json()
                 try:
-                    response_json = response.json()
-                    print(f"-------------------------------------------------------------\n{response_json}")
                     await send_large_message(ctx, response_json["choices"][0]["message"]["content"])
                 except Exception as e:
                     print(f"Error sending message: {e}")
+            elif response.status_code == 429:
+                await ctx.send("Aïe, je reçois trop de requêtes, réessaye 🤕")
             else:
                 print(f"Error: API returned status code {response.status_code}")
-                await ctx.send("An error occurred while processing your request. Please try again later.")
 
-
+    def get_response(self, headers, data):
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers=headers,
+            data=json.dumps(data),
+        )
+        return response
 
 async def send_large_message(ctx, message):
     message_parts = []
